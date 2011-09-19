4d346ace4ba13cd9818e444ba214a32762983929
Ricardo Signes <rjbs@cpan.org>
2011-09-19 10:04:16 -0400

    implement per-country overrides
    (this gets us fake countries, too, mostly)

--- lib/DateTimeX/TZPicker.pm
+++ lib/DateTimeX/TZPicker.pm
@@ -36,6 +36,7 @@ has _countries => (
 has _zone_lookup => (
   is   => 'ro',
   isa  => 'HashRef[ HashRef ]',
+  lazy => 1,
   init_arg => undef,
   builder  => '_build_zone_lookup',
   # traits   => [ 'Hash' ],
@@ -45,7 +46,7 @@ has _zone_lookup => (
 sub _build_zone_lookup {
   my ($self) = @_;
 
-  my $now = DateTime->now;
+  my $now = $self->constructed_at;
 
   my %zone;
   for my $tz (
@@ -69,7 +70,7 @@ sub _build_zone_lookup {
 # Eastern Time => America/New_York
 has country_overrides => (
   is  => 'ro',
-  isa => 'HashRef[ HashRef ]',
+  isa => 'HashRef[ ArrayRef ]',
   default => sub {  {}  },
   traits  => [ 'Hash' ],
   handles => {
@@ -81,18 +82,27 @@ sub zones_for_country {
   my ($self, $cc) = @_;
   $cc = lc $cc;
 
-  my %zone;
+  my @zones;
 
   if (my $override = $self->country_override_for($cc)) {
-    %zone = %$override;
+    # We have overridden the TZs for this country!
+    for (my $i = 0; $i < $#$override; $i += 2) {
+      push @zones, DateTimeX::TZPicker::TZ->_new({
+        %{ $self->_zone_lookup->{ $override->[ $i + 1 ] } },
+        virtual_name => $override->[ $i ],
+      });
+    }
   } else {
-    my $zfc = $self->_zones_for_country->{ $cc };
-    %zone  = map { $_ => $_ } @$zfc;
+    # Use whatever the database says.
+    @zones =
+      sort { $a->offset <=> $b->offset || $a->real_name cmp $b->real_name }
+      map  {; DateTimeX::TZPicker::TZ->_new({
+        %{ $self->_zone_lookup->{ $_ } }
+      }) }
+      @{ $self->_zones_for_country->{ $cc } },
   }
 
-  return
-    map {; +{ %{ $self->_zone_lookup->{ $zone{$_} } }, display_name => $_ } }
-    keys %zone;
+  return @zones;
 }
 
 has _zones_for_country => (
@@ -157,8 +167,24 @@ sub recent_instance {
 
 sub BUILD {
   my ($self) = @_;
+  $self->_zone_lookup;
   $self->_zones_for_country;
   $self->_all_zones;
+}
+
+{
+  package
+    DateTimeX::TZPicker::TZ;
+
+  sub _new { my ($class, $arg) = @_; bless {%$arg} => $class; }
+  sub offset       { $_[0]{offset}       }
+  sub offset_str   { $_[0]{offset_str}   }
+  sub real_name    { $_[0]{name}         }
+  sub virtual_name { $_[0]{virtual_name} || $_[0]{name} }
+
+  sub display_name {
+    return sprintf '(%s) %s', $_[0]->offset_str, $_[0]->virtual_name;
+  }
 }
 
 __PACKAGE__->meta->make_immutable;
--- t/basic.t
+++ t/basic.t
@@ -4,21 +4,53 @@ use warnings;
 use DateTimeX::TZPicker;
 use Test::More;
 
-my $tzpicker = DateTimeX::TZPicker->new({
-  country_overrides => {
-    us => {
-      'Eastern Time'  => 'America/New_York',
-      'Pacific Time'  => 'America/Los_Angeles',
-      'Central Time'  => 'America/Chicago',
-      'Mountain Time' => 'America/Denver',
-      'Alaskan Time'  => 'America/Anchorage',
-      'Hawaiian Time' => 'Pacific/Honolulu',
-    },
-  }
-});
+subtest "override for 'us'" => sub {
+  my $tzpicker = DateTimeX::TZPicker->new({
+    country_overrides => {
+      us => [
+        'Hawaiian Time'          => 'Pacific/Honolulu',
+        'Alaskan Time'           => 'America/Anchorage',
+        'Pacific Time'           => 'America/Los_Angeles',
+        'Mountain Time (no DST)' => 'America/Phoenix',
+        'Mountain Time'          => 'America/Denver',
+        'Central Time'           => 'America/Chicago',
+        'Eastern Time'           => 'America/New_York',
+      ],
+    }
+  });
 
-my @zfc = $tzpicker->zones_for_country('us');
+  my @zfc = $tzpicker->zones_for_country('us');
 
-note explain \@zfc;
+  is_deeply(
+    [ map {; $_->virtual_name } @zfc ],
+    [
+      'Hawaiian Time',
+      'Alaskan Time',
+      'Pacific Time',
+      'Mountain Time (no DST)',
+      'Mountain Time',
+      'Central Time',
+      'Eastern Time',
+    ],
+    'the US time zones: nice and simple',
+  );
+};
+
+subtest "no overrides" => sub {
+  my $tzpicker = DateTimeX::TZPicker->new;
+
+  my @zfc = $tzpicker->zones_for_country('us');
+
+  # We don't test for an exact set match because it would be too fragile.  Once
+  # Texas breaks away and declares its own time zone, our tests would fail with
+  # the next DateTime::TimeZone release.  Instead, we'll just test for
+  # something nice and simple. -- rjbs, 2011-09-19
+  ok(
+    (grep { $_->virtual_name eq 'America/Detroit' } @zfc)
+    &&
+    (grep { $_->virtual_name eq 'America/New_York' } @zfc),
+    "without an override, 'us' contains both Detroit and NYC",
+  )
+};
 
 done_testing;
